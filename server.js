70f0d4c8bc34ad7e3b9aaa9102665828af7447b1
Brad Harris <bradharris512@gmail.com>
2018-01-30 10:56:55 -0600

    Fixed gamewindow route

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 //Install express server
 const express = require('express');
 const app = express();
+const router = express.router();
+
+router.get('/gamewindow', function(req, res) {
+  res.render('./src/app/game-window/game-window.component.html');
+});
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
