3927dda70bbe63f64a2d3749c984a046340e1f19
Brad Harris <bradharris512@gmail.com>
2018-01-30 11:19:28 -0600

    Fixed gamewindow route try 4

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 //Install express server
 const express = require('express');
 const app = express();
-const router = express.router();
+//const router = express.router();
+// const path = require('path');
 
 // router.get('/gamewindow', function(req, res) {
 //   res.render('./src/app/game-window/game-window.component.html');
 // });
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+ app.use(express.static(__dirname + '/dist'));
+// app.get('*', (req, res) => {
+
+//   res.sendFile(path.join(__dirname, 'dist/index.html'));
+// });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
